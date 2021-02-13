dbe45e6d3504a41cfed6e9c78f441a98a7f1df1b
muku866 <79005233+muku866@users.noreply.github.com>
2021-02-13 19:53:39 +0900

    ああああ

---
+++ MyFirstAplication/src/main/java/com/example/app/SampleController.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+	@GetMapping("/test")
+	public String test(Model model) {
+		model.addAttribute("title","info");
+		return "test";
+	}
+}
---
+++ MyFirstAplication/src/main/java/com/example/demo/MyFirstAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstAplicationApplication.class, args);
+	}
+
+}
